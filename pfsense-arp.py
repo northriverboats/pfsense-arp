b5e8446825caf59536baa28c68e5515880347e6e
Fredrick W Warren <fredwarren@gmail.com>
2021-09-14 15:57:17 -0700

    updated detect correct path

--- pfsense-arp.py
+++ pfsense-arp.py
@@ -15,10 +15,13 @@ from paramiko import SSHClient, SSHException
 # =================================================================
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    return os.path.join(
-        os.environ.get( "_MEIPASS2", os.path.abspath(".")),
-        relative_path
-    )
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 # =================================================================
 # SQLITE RELEATED FUNCTIONS
